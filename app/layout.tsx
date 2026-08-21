c2134eef8c582bbbad4c0db2bbc1b3ac79bf157a
guy ambar <guya@local>
2026-08-21 14:01:02 +0200

    Add Cloudflare Web Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://hitscounter.dev; connect-src 'self' https://dummyjson.com; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'none';" />
+        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://hitscounter.dev; connect-src 'self' https://dummyjson.com https://static.cloudflareinsights.com; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'none';" />
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
@@ -26,6 +26,11 @@ export default function RootLayout({
       </head>
       <body>
         {children}
+        <script
+          type="module"
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "136e08eeda5c4767885f149528781334"}'
+        />
       </body>
     </html>
   );
